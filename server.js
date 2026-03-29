024158e2f6d994b6b7dc512947f1e84b1f202ad5
BeeingDeveloper <com.official.akhter007@gmail.com>
2026-03-29 13:31:44 +0530

    SERVER.JS : #25 : TESTING API

--- server.js
+++ server.js
@@ -40,7 +40,22 @@ server.use(
 
 // MONGOOSE CONNECTION SETUP----------------------------------------------------------------------------------------------
 const startTime = Date.now();
-const FlashAuthDB = FlashAuthDBConnection(startTime);
+const FlashAuthDB = mongoose.createConnection(mongoDBUrl, {
+        maxPoolSize: 5,
+        serverSelectionTimeoutMS: 10000,
+        connectTimeoutMS: 5000, // CONNECTION TIMEOUT
+        autoIndex: false, 
+});
+
+
+    FlashAuthDB.on('connecting', () => console.log("🔄 Connecting to FlashAuth database..."));
+    FlashAuthDB.on('connected', () => console.log(`🌐✅ #3. Connected to FlashAuth database in ${Date.now() - startTime} ms`));
+    FlashAuthDB.on('error', (error) => console.error("❌ Connection failed to FlashAuth DB:", error.message, new Date().toISOString()));
+    FlashAuthDB.on('disconnected', () => console.warn("⚠️ Disconnected from FlashAuth DB at", new Date().toISOString()));
+    FlashAuthDB.on('reconnected', () => console.log("🔄 Reconnected to FlashAuth DB at", new Date().toISOString()));
+
+    FlashAuthDB.asPromise().catch((err) => console.error("❌ FlashAuthDB init error:", err.message, new Date().toISOString()));
+
 
 
 // DATABASE CONNENTION HANDLER---------------------------------------------------------------------------------------------------------------------------------------
