80e091e54eeab346a8f4cea21bf403923c1c0e2e
BeeingDeveloper <com.official.akhter007@gmail.com>
2026-04-17 20:08:41 +0530

    AUTH STRATEGY IMPORT FIX

--- Authentication/AuthFactory.ts
+++ Authentication/AuthFactory.ts
@@ -1,21 +1,21 @@
-import AuthStrategy  from "../Interface/IAuthStrategy.ts";    
+import IAuthStrategy from "../Interface/IAuthStrategy.ts";    
 import { AuthType } from "../utils/AuthType.ts";
 
 
 export class AuthFactory{
-    public readonly authMap = new Map<AuthType, AuthStrategy>();
+    public readonly authMap = new Map<AuthType, IAuthStrategy>();
 
-    public constructor(authStrategyList: Array<AuthStrategy>){
+    public constructor(authStrategyList: Array<IAuthStrategy>){
         this.PopulateAuthMap(authStrategyList);
     }
 
-    private PopulateAuthMap=(authStrategyList: Array<AuthStrategy>):void=>{
+    private PopulateAuthMap=(authStrategyList: Array<IAuthStrategy>):void=>{
         authStrategyList.forEach((strategy)=>{
             this.authMap.set(strategy.getAuthType(), strategy);
         });
     }
 
-    public getStrategy(type: AuthType): AuthStrategy{
+    public getStrategy(type: AuthType): IAuthStrategy{
         return this.authMap.get(type);
     }
 }
