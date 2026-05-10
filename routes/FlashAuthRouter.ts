9fcbbec5adf96917ed18c8c96762b47208b2510c
Nasim Akhter <93286210+jagwar7@users.noreply.github.com>
2026-05-10 20:51:44 +0530

    Set Cross-Origin-Opener-Policy in success response

--- routes/FlashAuthRouter.ts
+++ routes/FlashAuthRouter.ts
@@ -253,6 +253,7 @@ router.get('/google/callback', async(req, res)=>{
         //----------------------------------------------------------------
         // ✅ SUCCESS RESPONSE
         console.log("SUCCESS: RESPONSE");
+        res.set("Cross-Origin-Opener-Policy" : "same-origin-allow-popups");
         return res.set('Content-Type', 'text/html').send(renderHTML);
         //----------------------------------------------------------------
 
